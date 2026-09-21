815731abc34e162da926992e07400060f2701f06
Lovfu <abc3469494886@163.com>
2026-09-21 17:23:43 +0800

    chore: 删除多余的笔记文件

--- internal/repository/lost_item_repo.go
+++ internal/repository/lost_item_repo.go
@@ -1,1 +1,33 @@
 package repository
+
+import (
+	"gin-demo/internal/model"
+
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Init(db *gorm.DB) {
+	DB = db
+}
+
+func CreateLostItem(item *model.LostItem) error {
+	return DB.Create(item).Error
+}
+
+func ListLostItems(location string) ([]model.LostItem, error) {
+	var items []model.LostItem
+	query := DB.Model(&model.LostItem{})
+	if location != "" {
+		query = query.Where("location LIKE ?", "%"+location+"%")
+	}
+	err := query.Find(&items).Error
+	return items, err
+}
+
+func GetLostItemByID(id string) (model.LostItem, error) {
+	var item model.LostItem
+	err := DB.First(&item, id).Error
+	return item, err
+}
